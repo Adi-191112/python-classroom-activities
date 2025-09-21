467dd5c981c4534facc2c1f5ba0d72e26a0701c1
Adi-191112 <adityaborkar1911@gmail.com>
2025-09-21 13:02:57 +0200

    sum.py

---
+++ loops.py
@@ -0,0 +1,12 @@
+S = "hello"
+#mention the loop= for, while
+# initialize the loop variable
+for i in S:
+    print(i)
+
+a = "Coding"    
+for i in a:
+    print(i)
+
+for i in range(30, 40, 3):
+    print(i)
---
+++ sum.py
@@ -0,0 +1,6 @@
+r = int(input("enter a number"))
+sum = 0 
+for i in range(1, r+1, 1):
+    sum = sum + i
+    
+print(sum)
