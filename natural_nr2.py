19ad21feaa8901f409b8317c2f79a97ed1fe50e4
Adi-191112 <adityaborkar1911@gmail.com>
2025-10-01 18:08:03 +0200

    while lloop

---
+++ natural_nr2.py
@@ -0,0 +1,10 @@
+num = int(input("enter a number"))
+ 
+sum = 0
+i = 1
+ 
+while i<=num:
+    sum = sum + i
+    i=i+1
+
+print(sum)
--- sum.py
+++ sum.py
@@ -3,4 +3,4 @@ sum = 0
 for i in range(1, r+1, 1):
     sum = sum + i
     
-print(sum)
+-print(sum)
