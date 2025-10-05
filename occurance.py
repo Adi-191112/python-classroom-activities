494d5f72f9635e349a3af80bde1b858bdcfb0137
Adi-191112 <adityaborkar1911@gmail.com>
2025-10-05 12:36:59 +0200

    occurance

---
+++ occurance.py
@@ -0,0 +1,13 @@
+word = input("Enter the word")
+letter = input("Enter the letter")
+i = 0
+count = 0
+while(i < len(word)):
+    if(word[i] == (letter)):
+        count = count + 1 
+    
+    i = i + 1
+
+    
+
+print("The count is: ", count)    
