984afbed2f136a8c333ee6d9ab2129867f22d208
Adi-191112 <adityaborkar1911@gmail.com>
2025-11-30 11:47:09 +0100

    weather

---
+++ tuple.py
@@ -0,0 +1,20 @@
+# create tuple with different datatypes
+t1 = (1,-54,"hello",2.4,False)
+print(t1)
+
+# create tuple with int datatype
+t2 = (2,-3,36,9,-6,)
+print(t2)
+
+# add 9 to int tuple
+t2 = t2 + (9,10)
+print(t2)
+#count occurences
+t3 = (4,8,52,4,9,8,1,8)
+print(t3.count(4))
+t4 = (4,46,6,6,"cool",5070,7800,3,'d','x',3,5,7,7,8)
+print(t4[2:6])
+
+#slicing
+
+
