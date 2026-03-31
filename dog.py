096f9727bd157ecadbda89505554d485f03ef697
Adi-191112 <adityaborkar1911@gmail.com>
2026-03-31 13:17:45 +0200

    dog activity

---
+++ dog.py
@@ -0,0 +1,12 @@
+class dog:
+
+    def __init__(self,breed,color):
+        self.breed = breed
+        self.color = color
+
+
+
+dog1 = dog("golden retriever", "blue")
+dog2 = dog ("german shepard","red")
+print ( " the breed is " , dog2.breed, " whose color is " , dog2.color)
+print( " the breed is ", dog1.breed, " whose color is ",dog1.color)
